1ba941c5a74a37704df9ba26b35dfcaf23d53192
Shirajuki <shirajuki00@gmail.com>
2022-02-19 16:02:17 +0100

    feat: adds finalized styling and fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,36 @@ import { useRef, useState } from 'react';
 import './App.css';
 import { SlotMachine } from '@lucky-canvas/react';
 import Reward from 'react-rewards';
+import { CornerPiece } from './CornerPiece';
 
 const initialState = {
   blocks: [
-    { padding: '10px', background: '#617df2', borderRadius: '10px' },
-    { padding: '10px', background: '#869cfa', borderRadius: '20px' },
-    { padding: '10px', background: '#afc8ff', borderRadius: '40px' },
-    { padding: '20px 0px', background: '#e9e8fe', borderRadius: '25px' },
+    { padding: '10px', background: '#E83939', borderRadius: '10px' },
+    { padding: '10px', background: '#F15454', borderRadius: '20px' },
+    { padding: '10px', background: '#E86A6A', borderRadius: '40px' },
+    { padding: '20px 0px', background: '#FEA0A0', borderRadius: '25px' },
   ],
   prizes: [
     {
+      name: 'jonny',
       fonts: [{ text: 'Jonny', top: '43%' }],
-      background: '#b8c5f2',
+      background: '#F2CF76',
       borderRadius: '25px',
     },
     {
-      fonts: [{ text: 'Kelvin', top: '43%' }],
-      background: '#b8c5f2',
-      borderRadius: '25px',
-    },
-    {
-      fonts: [{ text: 'Jimmy', top: '43%' }],
-      background: '#b8c5f2',
-      borderRadius: '25px',
-    },
-    {
+      name: 'eline',
       fonts: [{ text: 'Eline', top: '43%' }],
-      background: '#b8c5f2',
+      background: '#F2CF76',
+      borderRadius: '25px',
+    },
+    {
+      name: 'kelvin',
+      fonts: [{ text: 'Kelvin', top: '43%' }],
+      background: '#F2CF76',
       borderRadius: '25px',
     },
   ],
-  slots: [{ order: [0, 1, 0, 2, 3], direction: -1 }],
+  slots: [{ order: [0, 1, 2, 0, 0], direction: -1 }],
   defaultConfig: { mode: 'horizontal', rowSpacing: '10px', colSpacing: '10px' },
 };
 
@@ -40,11 +39,59 @@ const App = () => {
   const [state, setState] = useState(initialState);
   const slotMachine = useRef(null);
   const reward = useRef(null);
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [lastWon, setLastWon] = useState(-1);
+  const [show1, setShow1] = useState(true);
+  const [show2, setShow2] = useState(false);
+
+  const start = () => {
+    // Delete order from slot lastwin
+    if (lastWon > -1) {
+      for (let i = 0; i < state.slots[0].order.length; i++) {
+        if (state.slots[0].order[i] === lastWon)
+          state.slots[0].order.splice(i, 1);
+      }
+      for (let i = 0; i < state.slots[0].order.length; i++) {
+        if (state.slots[0].order[i] === lastWon)
+          state.slots[0].order.splice(i, 1);
+      }
+    }
+    slotMachine.current.play();
+  };
   const spin = () => {
     const slots = state.slots[0].order;
     const index = slots[(Math.random() * slots.length) >> 0];
     slotMachine.current.stop(index);
     setTimeout(() => reward.current.rewardMe(), 2550);
+    setLastWon(index);
+    console.log(index, state.prizes[index].name);
+  };
+  const addPrize = (event) => {
+    event.preventDefault();
+    const prize = {
+      name: id,
+      fonts: [{ text: name, top: '43%' }],
+      background: '#F2CF76',
+      borderRadius: '25px',
+    };
+    const dup = state.prizes.map(({ name }) => name).indexOf(id);
+    let index = state.prizes.length;
+    if (dup == -1) {
+      state.prizes.push(prize);
+    } else {
+      index = dup;
+    }
+    state.slots[0].order.push(index);
+    console.log(prize, state.prizes, state.slots[0].order);
+  };
+  const toggle1 = () => {
+    setShow1(!show1);
+    console.log('a', show1);
+  };
+  const toggle2 = () => {
+    setShow2(!show2);
+    console.log('b', show2);
   };
   return (
     <>
@@ -81,8 +128,53 @@ const App = () => {
             }}
           ></SlotMachine>
         </div>
-        <button onClick={() => slotMachine.current.play()}>Start</button>
-        <button onClick={spin}>Stop</button>
+        <div className="formWrapper" hidden={show1}>
+          <div className="buttonWrapper">
+            <button onClick={start}>Start</button>
+            <button onClick={spin}>Stop</button>
+          </div>
+          <form onSubmit={addPrize} className="form">
+            <label>
+              <input
+                type="text"
+                value={id}
+                placeholder="ID"
+                onChange={(ev) => setId(ev.target.value)}
+              />
+            </label>
+            <label>
+              <input
+                type="text"
+                value={name}
+                placeholder="Name"
+                onChange={(ev) => setName(ev.target.value)}
+              />
+            </label>
+            <input type="submit" value="Add lottery" />
+          </form>
+        </div>
+      </div>
+      <div className="formWrapper down" hidden={show2}>
+        {state.prizes.map((prize, index) => {
+          const name = prize.fonts[0].text;
+          const count = state.slots[0].order.filter((o) => o === index).length;
+          return (
+            <div className="info">
+              <p>
+                ({prize.name}) {name} {count}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <img src="./pick.png" alt="Scroll pick" className="pick" />
+      <CornerPiece className="corner topleft" />
+      <CornerPiece className="corner topright" />
+      <CornerPiece className="corner bottomleft" />
+      <CornerPiece className="corner bottomright" />
+      <div className="topButton">
+        <button onClick={() => toggle1()}></button>
+        <button onClick={() => toggle2()}></button>
       </div>
     </>
   );
---
+++ src/CornerPiece.jsx
@@ -0,0 +1,123 @@
+export const CornerPiece = ({ className }) => (
+  <svg
+    width="197"
+    height="197"
+    viewBox="0 0 197 197"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <rect width="31" height="5" fill="#F2CF76" />
+    <rect x="1" y="72" width="31" height="5" fill="#F2CF76" />
+    <rect y="113" width="24" height="5" fill="#F2CF76" />
+    <rect y="137" width="24" height="5" fill="#F2CF76" />
+    <rect x="118" y="19" width="24" height="5" fill="#F2CF76" />
+    <rect y="87" width="48" height="5" fill="#F2CF76" />
+    <rect x="45" y="72" width="31" height="5" fill="#F2CF76" />
+    <rect x="46" width="31" height="5" fill="#F2CF76" />
+    <rect x="87" width="31" height="5" fill="#F2CF76" />
+    <rect y="27" width="77" height="5" fill="#F2CF76" />
+    <rect y="43" width="90" height="5" fill="#F2CF76" />
+    <rect x="137" width="60" height="5" fill="#F2CF76" />
+    <rect
+      y="197"
+      width="60"
+      height="5"
+      transform="rotate(-90 0 197)"
+      fill="#F2CF76"
+    />
+    <rect
+      y="32"
+      width="31"
+      height="5"
+      transform="rotate(-90 0 32)"
+      fill="#F2CF76"
+    />
+    <rect
+      y="77"
+      width="31"
+      height="5"
+      transform="rotate(-90 0 77)"
+      fill="#F2CF76"
+    />
+    <rect
+      y="118"
+      width="31"
+      height="5"
+      transform="rotate(-90 0 118)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="72"
+      y="32"
+      width="31"
+      height="5"
+      transform="rotate(-90 72 32)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="113"
+      y="24"
+      width="24"
+      height="5"
+      transform="rotate(-90 113 24)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="137"
+      y="24"
+      width="24"
+      height="5"
+      transform="rotate(-90 137 24)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="19"
+      y="142"
+      width="24"
+      height="5"
+      transform="rotate(-90 19 142)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="87"
+      y="48"
+      width="48"
+      height="5"
+      transform="rotate(-90 87 48)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="72"
+      y="75"
+      width="31"
+      height="5"
+      transform="rotate(-90 72 75)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="72"
+      y="77"
+      width="31"
+      height="5"
+      transform="rotate(-90 72 77)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="27"
+      y="77"
+      width="77"
+      height="5"
+      transform="rotate(-90 27 77)"
+      fill="#F2CF76"
+    />
+    <rect
+      x="43"
+      y="92"
+      width="92"
+      height="5"
+      transform="rotate(-90 43 92)"
+      fill="#F2CF76"
+    />
+  </svg>
+);
